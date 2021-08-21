e2eec443e6fe1fdc24651b00305c69b6e4c4ae35
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-21 20:39:57 +0530

    If else statement to find even-odd number

---
+++ if .py
@@ -0,0 +1,10 @@
+#input: enter a number 20
+#output:even number
+#description:If else statement to find even-odd number
+#date: 21-08-2021
+#Author name: Shruti Nahar
+no=int(input("enter a number"))
+if(no%2==0):
+  print("even number")
+else:
+    print("odd number")
