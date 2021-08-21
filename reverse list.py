6621a11e8f419bc0b70b33fada1188322a38612b
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-21 20:42:28 +0530

    Reversing the list

---
+++ reverse list.py
@@ -0,0 +1,10 @@
+#input:-list=[ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+#output:-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#        [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+#description:Reversing the list
+#date: 21-08-2021
+#Author name: Shruti Nahar
+list=[ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+print(list)
+reverse_list=list[::-1]
+print(reverse_list)
