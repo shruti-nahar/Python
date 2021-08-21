2ef85ffe79076086b2ebc7a458fb287915e1ef45
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-21 20:40:36 +0530

    If elif else statement

---
+++ if elif else.py
@@ -0,0 +1,14 @@
+#input:--
+#output:Grade B
+#description:If elif else statement 
+#date: 21-08-2021
+#Author name: Shruti Nahar
+marks=75
+if(marks>80):
+    print("Grade A")
+elif (marks>60) and (marks<=80):
+    print("Grade B")
+elif(marks>40) and (marks<=60):
+    print("Grade C")
+else:
+    print("Fail")
