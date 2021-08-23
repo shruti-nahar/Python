9bf80fdf56bc1643b139870be13e67216594f6f0
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-23 08:26:50 +0530

    use of break statement

---
+++ break statement.oy.py
@@ -0,0 +1,11 @@
+#input:- --
+#output:-0 1 2 3 4 5 6 7 8 9 10
+#description:-Implementation of Break Statement
+#date: 23-08-2021
+#Author name: Shruti Nahar
+count=0;
+while True:
+    print(count)
+    count+=1
+    if(count>10):
+        break
