3165f783039ef0f89d07ea6f4e90c5195d0e54da
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-24 11:00:14 +0530

    Arithmetic Operation

--- Arithmeticoperator.py
+++ Arithmeticoperator.py
@@ -1,3 +1,14 @@
+'''input:--
+output:Num1 + Num2=  30
+       Num1 - Num2=  -10
+       Num1 * Num2=  200
+       Num1 / Num2=  0.5
+       5^3=  125
+       20%3=  2
+       22//7=  3
+description:perform Arithmetic Operation
+date: 24-08-2021
+Author name: Shruti Nahar'''
 num1=10
 num2=20
 print("Num1 + Num2= ",num1+num2)
