b784c75bf71f1a07ea64ce46026e242d145740c3
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-24 10:12:41 +0530

    continue statement

---
+++ continue.py
@@ -0,0 +1,15 @@
+#input:--
+#output:  1 pune mumbia 2 3 4
+#description:continue statement
+#date: 24-08-2021
+#Author name: Shruti Nahar
+a=1
+while(a<5):
+    print(a)
+    a=a+1
+    if(a>2):
+        continue
+    print("pune")
+    print("mumbia")
+        
+
