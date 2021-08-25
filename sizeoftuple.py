57265b531e4c093af5f3a7de725657f85e94e416
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-25 10:28:06 +0530

    Find the size of tuples in bytes

---
+++ sizeoftuple.py
@@ -0,0 +1,7 @@
+#input:- --
+#output:-size of tuple: 48
+#description:-Find the size of tuples in bytes
+#date: 25-08-2021
+#Author name: Shruti Nahar
+tuple=("shruti",21,"abc")
+print("size of tuple: "+str(tuple.__sizeof__()))
