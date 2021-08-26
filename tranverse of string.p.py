a66ad70cdf97fc19c2246c44b6ef0f25e6e1d17c
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-26 10:22:32 +0530

    Transversing of String

---
+++ tranverse of string.p.py
@@ -0,0 +1,26 @@
+'''input:good morning
+output:-
+g
+o
+o
+d
+
+m
+o
+r
+n
+i
+n
+g
+description: Transversing of the string
+date: 26-08-2021
+Author name: Shruti Nahar'''
+msg="good morning"
+index=0
+while index<len(msg):
+     letter=msg[index]
+     print(letter)
+     index=index+1
+
+
+
