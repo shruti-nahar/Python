f98ba5a6bcf1abe4cee26fb37664b78a6780ca9d
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-27 09:06:10 +0530

    Check the string is palidrome or not

---
+++ palidrome.py
@@ -0,0 +1,12 @@
+'''input:- madam
+output:-Palidrome
+description: Check the string is palidrome or not
+date: 27-08-2021
+Author name: Shruti Nahar'''
+def palidrome(num):
+    x=num[::-1]
+    if(x==num):
+        print("Palidrome")
+    else:
+        print("Not a Palidrome ")
+palidrome("madam")
