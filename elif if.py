0fe5a15d565b1dc83627a20ca30c6100ad41b3da
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-28 14:54:34 +0530

    Check number is positive negative or zero

---
+++ elif if.py
@@ -0,0 +1,13 @@
+'''input:enter a number-1
+output:--1 is negative  
+description; Check number is positive negative or zero
+date: 28-08-2021
+Author name: Shruti Nahar'''
+no=int(input("enter a number"))
+
+if(no>0):
+ print(no,"is positive")
+elif(no<0):
+    print(no,"is negative")
+elif(no==0):
+   print("equal to zero")
