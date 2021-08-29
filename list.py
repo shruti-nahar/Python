a4abc3fc3252b35e0f4915fbc43f5ade350889bd
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-29 22:07:16 +0530

    Implementation of List

--- list.py
+++ list.py
@@ -1,3 +1,13 @@
+'''
+Input:-
+Output:-
+append()  ['shruti', 21, 'shubham', 'prachi']
+extend()  ['shruti', 21, 'shubham', 'prachi', 'shiva', 'om']
+insert()  ['shruti', 21, 'sheetal', 'shubham', 'prachi', 'shiva', 'om']
+pop()  om
+Description :-Implementation of List
+Date:-29-08-2021
+Author: Shruti Nahar'''
 list=['shruti',21,'shubham']
 list.append("prachi")
 print("append() ",list)
