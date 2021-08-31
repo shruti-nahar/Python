771fec6e5ebbc20ae12e487731699782a05b83fa
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-31 12:14:50 +0530

    Implementation of function

---
+++ function.py
@@ -0,0 +1,10 @@
+'''input:--
+output:-shruti nsahar
+        abc
+description:-Implementation of function
+date: 31-08-2021
+Author name: Shruti Nahar'''
+def sample():
+   print ("shruti nsahar")
+   print ("abc")
+sample()
