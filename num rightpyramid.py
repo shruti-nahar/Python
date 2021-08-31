fb0cba71b35e1209f31a334532fc7960bea10599
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-08-31 12:12:29 +0530

    numeric half right Pyramid pattern

--- num rightpyramid.py
+++ num rightpyramid.py
@@ -1,10 +1,10 @@
 '''input:--
 output:- 
 1  
-2 3  
-4 5 6  
-7 8 9 10        
-11 12 13 14 15  
+2 2  
+3 3 3
+4 4 4 4
+5 5 5 5 5
 description:numeric right Pyramid pattern
 date: 31-08-2021
 Author name: Shruti Nahar'''
