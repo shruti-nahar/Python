db31c331032ef4657a366757f2fa80f0b220465d
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-03 10:26:53 +0530

    reverse pyramid pattern

---
+++ reverse pyramid.py
@@ -0,0 +1,11 @@
+
+def pattern(n):
+    k=(2*n)-2
+    for i in range(n,-1,-1):
+        for j in range(0,k):
+            print(end=" ")
+        k=k-1
+        for j in range(0,i+1):
+            print("*",end=' ')
+        print("\n")
+pattern(10)
