757c5dd25d8bf9d4a0b7318213c2f0e1496a523b
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-11 18:48:31 +0530

    Add files via upload

---
+++ perfect.p.py
@@ -0,0 +1,17 @@
+'''input:-lower bound:20
+          upper bound:30
+output:- perfect number  28
+description:check for perfect 
+date: 11-09-2021
+Author name: Shubham Lodha'''
+def perfect(l,u):
+     for num in range(l,u):
+          sum=0
+          for i in range(1,num):
+               if(num%i)==0:
+                    sum=sum+i
+          if(sum==num):
+               print("perfect number ",sum)
+l=int(input("lower bound:"))
+u=int(input("upper bound:"))
+perfect(l,u)
